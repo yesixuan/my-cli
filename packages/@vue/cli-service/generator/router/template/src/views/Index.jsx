71369225dede3bafcfe1e496145417e6937a852c
yesixuan <18373209286@163.com>
2019-07-02 18:48:04 +0800

    feta: 完整的路由配置支持（包括history模式的选择）

---
+++ packages/@vue/cli-service/generator/router/template/src/views/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Index = () => (
+  <div className="App">
+    <header className="App-header">
+      <h3>Index page</h3>
+    </header>
+  </div>
+)
+
+export default Index
